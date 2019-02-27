cc665d7773fe192d7c5e5f1644ba2fdf0bb9cd80
spikeburton <ralphburton3@gmail.com>
2019-02-27 17:04:29 -0500

    generate seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Cake.destroy_all
+
+spike = User.create!(name: "Spike Burton", email: "spikeburton@hotmail.com")
+dana = User.create!(name: "Dana Simmons", email: "danasimmons@yahoo.com")
+leigh = User.create!(name: "Leigh Baugham", email: "leighbaugham@aol.com")
+
+wedding = Cake.create!(name: "Wedding Special", flavor: "vanilla")
+carrot = Cake.create!(name: "Bugs Bunny", flavor: "carrot")
+chocolate = Cake.create!(name: "Bugs Bunny", flavor: "chocolate hazelnut")
+
+# spike.orders.create!()
