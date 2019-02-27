19d40444afe6cf27708aec0da8f569146e93e81a
spikeburton <ralphburton3@gmail.com>
2019-02-27 17:20:02 -0500

    add index and show pages for cake model

--- app/controllers/cakes_controller.rb
+++ app/controllers/cakes_controller.rb
@@ -1,2 +1,9 @@
 class CakesController < ApplicationController
+  def index
+    @cakes = Cake.all
+  end
+
+  def show
+    @cake = Cake.find(params[:id])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
 Cake.destroy_all
+Order.destroy_all
 
 spike = User.create!(name: "Spike Burton", email: "spikeburton@hotmail.com")
 dana = User.create!(name: "Dana Simmons", email: "danasimmons@yahoo.com")
@@ -14,6 +15,10 @@ leigh = User.create!(name: "Leigh Baugham", email: "leighbaugham@aol.com")
 
 wedding = Cake.create!(name: "Wedding Special", flavor: "vanilla")
 carrot = Cake.create!(name: "Bugs Bunny", flavor: "carrot")
-chocolate = Cake.create!(name: "Bugs Bunny", flavor: "chocolate hazelnut")
+chocolate = Cake.create!(name: "Choco Waka", flavor: "chocolate hazelnut")
 
 # spike.orders.create!()
+Order.create!(user_id: spike.id, cake_id: carrot.id, order_date: DateTime.now, fulfilled: false)
+Order.create!(user_id: spike.id, cake_id: chocolate.id, order_date: DateTime.now, fulfilled: true)
+Order.create!(user_id: dana.id, cake_id: wedding.id, order_date: DateTime.now, fulfilled: false)
+Order.create!(user_id: leigh.id, cake_id: wedding.id, order_date: DateTime.now, fulfilled: true)
