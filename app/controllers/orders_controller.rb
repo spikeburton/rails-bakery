156c08f31cd4c016dac1eb8f6a716e6c413eb9de
spikeburton <ralphburton3@gmail.com>
2019-02-27 20:47:51 -0500

    add views for order model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,26 @@
 class OrdersController < ApplicationController
+  def index
+    @orders = Order.all.sort_by { |o| o.user.name }
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(user_id: params[:order][:user], cake_id: params[:order][:cake],
+      order_date: DateTime.now, fulfilled: false)
+
+    if @order.valid?
+      binding.pry
+      @order.save
+      redirect_to order_path(@order)
+    else
+      render :new
+    end
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :cake
+
+  def order_date_str
+    self.order_date.strftime("%B %e, %Y at %l:%M %P")
+  end
+
+  def fulfilled_str
+    self.fulfilled ? "Yes" : "No"
+  end
 end
