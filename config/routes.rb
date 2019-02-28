21db4862552615b451d136c957edfb715cad5042
spikeburton <ralphburton3@gmail.com>
2019-02-27 19:36:44 -0500

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :orders
+  resources :orders, only: [:index, :show, :new, :create]
   resources :cakes, only: [:index, :show]
-  resources :users
+  resources :users, only: [:show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
